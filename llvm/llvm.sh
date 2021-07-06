b084843fdeca68156d707b7610ea0c357b103487
Mircho Rodozov <mrodozov@cern.ch>
2021-07-07 00:52:19 +0200

    update llvm files

--- llvm/llvm.sh
+++ llvm/llvm.sh
@@ -8,14 +8,5 @@ G77_ROOT=$GCC_ROOT
 export GCC_ROOT
 export G77_ROOT
 
-cat << \EOF_TOOLFILE >${toolfolder}/llvm-f77compiler.xml
-  <tool name="llvm-f77compiler" version="@TOOL_VERSION@" type="compiler">
-    <use name="gcc-f77compiler"/>
-    <client>
-      <environment name="FC" default="@G77_ROOT@/bin/gfortran"/>
-    </client>
-  </tool>
-EOF_TOOLFILE
-
 # General substitutions
 perl -p -i -e 's|\@([^@]*)\@|$ENV{$1}|g' ${toolfolder}/llvm-f77compiler.xml
