6a0618eb3e4637abee867add7e55e2269baa32cc
Mircho Rodozov <mrodozov@cern.ch>
2021-07-06 14:48:56 +0200

    fix llvm script and remove the macos part

--- llvm/llvm.sh
+++ llvm/llvm.sh
@@ -2,15 +2,9 @@
 
 toolfolder=${1}
 
-if [ "X$GCC_ROOT" = X ]
-then
-    GCC_PATH=`which gcc` || exit 1
-    GCC_VERSION=`gcc -dumpversion` || exit 1
-    GCC_ROOT=`echo $GCC_PATH | sed -e 's|/bin/gcc||'`
-    G77_ROOT=$GFORTRAN_MACOSX_ROOT
-else
-    G77_ROOT=$GCC_ROOT
-fi
+GCC_PATH=`which gcc` || exit 1
+GCC_ROOT=`echo $GCC_PATH | sed -e 's|/bin/gcc||'`
+G77_ROOT=$GCC_ROOT
 export GCC_ROOT
 export G77_ROOT
 
